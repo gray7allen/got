dec47042f4be58ee1177b8aaf4e69fc2c065aa43
Grayson Allen <grayallen1991@gmail.com>
2022-05-25 14:32:29 -0400

    jon snows health is down

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 let jonSnowAttack = 25;
-let jamieLannister = 35;
+let jonSnowHealth = 100;
+let jonSnowDefense = 0;
 
-if (jonSnowAttack > jamieLannister) {
+let jamieLannisterAttack = 25;
+
+if (jonSnowAttack > jamieLannisterAttack) {
     console.log("Jon snow has better attack than Jamie Lannister");
-} else {
+} else if (jonSnowAttack < jamieLannisterAttack) {
     console.log("Jamie Lannister has better attack than Jon Snow")
+} else {
+    console.log("Jon Snow and Jamie Lannister have the same attack")
 }
+
+if (jonSnowHealth <= jamieLannisterAttack) {
+    console.log("Jon Snow has been slain");
+    } else {
+        //jonSnowHealth = jonSnowHealth - jamieLannisterAttack
+        jonSnowHealth -= jamieLannisterAttack
+        console.log(`Jon Snow's health is down to ${jonSnowHealth}`);
+    }
