3f7e1b24f0adccc9f26c656dad664c19474fa0a2
Grayson Allen <grayallen1991@gmail.com>
2022-05-25 14:16:36 -0400

    initial commit: sizing up

---
+++ index.js
@@ -0,0 +1,8 @@
+let jonSnowAttack = 25;
+let jamieLannister = 35;
+
+if (jonSnowAttack > jamieLannister) {
+    console.log("Jon snow has better attack than Jamie Lannister");
+} else {
+    console.log("Jamie Lannister has better attack than Jon Snow")
+}
